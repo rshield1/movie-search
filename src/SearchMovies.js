cffa932d1be35acf07479b8af0864bae45277bb0
Robert Shields <robert.shieldsiv@gmail.com>
2020-08-27 21:28:54 -0400

    added hooks to react for state management

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,21 +1,27 @@
-import React from 'react'
+// Add react hooks to manage state in a functional component
+import React, { useState }from 'react'
 
 
 export default function SearchMovies(){
 // html for is used for a specific element 
+//states for input query, and movies
+// use state will return an array of 2 things...
+// the state and a function that will update the state
+    const [query, setQuery] = useState('');
+//create the state for movies, and update that state appropriate
+    const [movies, setMovies] = useState([]);
 
     const searchMovies = async (e) => {
         e.preventDefault();
-        
-        const query = "Gladiator"
-
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=615584ffa7f7ade097f6cf6fd0891d19&language=en-US&query=${query}&page=1&include_adult=false`;
-
+        const url = `https://api.themoviedb.org/3/search/movie?
+                    api_key=615584ffa7f7ade097f6cf6fd0891d19&
+                    language=en-US&query=${query}&page=1&include_adult=false`;
         try {
             const res = await fetch(url);
             const data  = await res.json();
-            console.log(data);
-            
+            console.log(data.results);
+            setMovies(data.results);
+
         }catch(err){
 
             console.error(err);
@@ -29,6 +35,8 @@ export default function SearchMovies(){
                 type="text" 
                 name='query' 
                 placeholder='Gladiator'
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
             />
             <button className="button" type="submit">Search</button>
         </form>
