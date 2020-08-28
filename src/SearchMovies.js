921fa786be71984ec762e7c6efc560cd97ee7595
Robert Shields <robert.shieldsiv@gmail.com>
2020-08-27 23:12:38 -0400

    added moviecard component

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,5 +1,6 @@
 // Add react hooks to manage state in a functional component
 import React, { useState }from 'react'
+import MovieCard from './MovieCard'
 
 
 export default function SearchMovies(){
@@ -7,38 +8,46 @@ export default function SearchMovies(){
 //states for input query, and movies
 // use state will return an array of 2 things...
 // the state and a function that will update the state
-    const [query, setQuery] = useState('');
+    const [query, setQuery] = useState('0');
 //create the state for movies, and update that state appropriate
+    // const [movies, setMovies] = useState([]);
+    
     const [movies, setMovies] = useState([]);
-
+    
     const searchMovies = async (e) => {
         e.preventDefault();
-        const url = `https://api.themoviedb.org/3/search/movie?
-                    api_key=615584ffa7f7ade097f6cf6fd0891d19&
-                    language=en-US&query=${query}&page=1&include_adult=false`;
+                
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=615584ffa7f7ade097f6cf6fd0891d19&language=en-US&query=${query}&page=1&include_adult=false`;
+        
         try {
             const res = await fetch(url);
             const data  = await res.json();
-            console.log(data.results);
             setMovies(data.results);
-
         }catch(err){
-
             console.error(err);
         }
-
     }
     return (
-        <form className="form" onSubmit={searchMovies}>
-            <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" 
-                type="text" 
-                name='query' 
-                placeholder='Gladiator'
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-            />
-            <button className="button" type="submit">Search</button>
-        </form>
+        <div>
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input className="input" 
+                    type="text" 
+                    name='query' 
+                    placeholder='Gladiator'
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button className="button" type="submit">Search</button>
+                
+            </form>
+
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie =>
+                   <MovieCard movie = {movie} key={movie.id} />
+                )}
+            </div>
+        </div>
+        
     )
 }
