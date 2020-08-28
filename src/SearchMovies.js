e0a505fb69d37959b94e91791f4a525d4662b75a
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-08-27 21:02:27 -0400

    added fetch request to api using async/await

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -3,8 +3,27 @@ import React from 'react'
 
 export default function SearchMovies(){
 // html for is used for a specific element 
+
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        
+        const query = "Gladiator"
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=615584ffa7f7ade097f6cf6fd0891d19&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            console.log(data);
+            
+        }catch(err){
+
+            console.error(err);
+        }
+
+    }
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" 
                 type="text" 
